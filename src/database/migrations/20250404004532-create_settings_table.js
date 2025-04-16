1d0a3b673b76f8445563f5636870f1fcef329d1a
Jorge Herrera <jorgeluishm88@gmail.com>
2025-04-16 12:39:18 -0300

    validations in setting migration

--- src/database/migrations/20250404004532-create_settings_table.js
+++ src/database/migrations/20250404004532-create_settings_table.js
@@ -3,40 +3,69 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
+    const { INTEGER, FLOAT, DATE, Op } = Sequelize;
+
     await queryInterface.createTable("settings", {
       id: {
-        type: Sequelize.INTEGER,
-        autoIncrement: true,
+        type: INTEGER,
         primaryKey: true,
         allowNull: false,
+        defaultValue: 1,
       },
       minBookingLength: {
-        type: Sequelize.INTEGER,
+        type: INTEGER,
         allowNull: false,
         defaultValue: 1,
       },
       maxBookingLength: {
-        type: Sequelize.INTEGER,
+        type: INTEGER,
         allowNull: false,
         defaultValue: 30,
       },
       breakfastPrice: {
-        type: Sequelize.FLOAT,
+        type: FLOAT,
         allowNull: false,
         defaultValue: 0.0,
       },
       createdAt: {
-        type: Sequelize.DATE,
+        type: DATE,
         allowNull: false,
       },
       updatedAt: {
-        type: Sequelize.DATE,
+        type: DATE,
         allowNull: false,
       },
     });
+
+    await queryInterface.addConstraint("settings", {
+      fields: ["minBookingLength"],
+      type: "check",
+      where: {
+        minBookingLength: { [Op.gte]: 1 },
+      },
+      name: "check_min_booking_length",
+    });
+
+    await queryInterface.addConstraint("settings", {
+      fields: ["maxBookingLength"],
+      type: "check",
+      where: {
+        maxBookingLength: { [Op.gte]: Sequelize.col("minBookingLength") },
+      },
+      name: "check_max_booking_length",
+    });
+
+    await queryInterface.addConstraint("settings", {
+      fields: ["breakfastPrice"],
+      type: "check",
+      where: {
+        breakfastPrice: { [Op.gte]: 0 },
+      },
+      name: "check_breakfast_price_positive",
+    });
   },
 
-  async down(queryInterface, Sequelize) {
+  async down(queryInterface) {
     await queryInterface.dropTable("settings");
   },
 };
