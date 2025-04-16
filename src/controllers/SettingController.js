6d4cac5a137af93f025c66bf4481c3cf3d07e3fc
Jorge Herrera <jorgeluishm88@gmail.com>
2025-04-15 23:16:42 -0300

    merge done

--- src/controllers/SettingController.js
+++ src/controllers/SettingController.js
@@ -1,9 +1,6 @@
 const Setting = require("../models/Setting");
 const settingValidation = require("../validations/settingValidation");
-<<<<<<< HEAD
 const MESSAGES = require("../utils/messages");
-=======
->>>>>>> 078610e6bb36b363a5af1cfd0141ccc142b96f71
 
 class SettingController {
   async getAllSettings(req, res) {
@@ -11,11 +8,7 @@ class SettingController {
       const settings = await Setting.findAll();
       res.json(settings);
     } catch (error) {
-<<<<<<< HEAD
       return res.status(500).json({ error: MESSAGES.GENERAL.SERVER_ERROR });
-=======
-      res.status(500).json({ error: "Error al obtener las configuraciones" });
->>>>>>> 078610e6bb36b363a5af1cfd0141ccc142b96f71
     }
   }
 
@@ -35,13 +28,9 @@ class SettingController {
           detalles: error.errors,
         });
       }
-<<<<<<< HEAD
       return res
         .status(500)
         .json({ error: MESSAGES.GENERAL.CREATE_ERROR("Configuração") });
-=======
-      res.status(500).json({ error: "Error al crear la configuración" });
->>>>>>> 078610e6bb36b363a5af1cfd0141ccc142b96f71
     }
   }
 
@@ -69,13 +58,9 @@ class SettingController {
           detalles: error.errors,
         });
       }
-<<<<<<< HEAD
       return res
         .status(500)
         .json({ error: MESSAGES.GENERAL.UPDATE_ERROR("Configuração") });
-=======
-      res.status(500).json({ error: "Error al actualizar la configuración" });
->>>>>>> 078610e6bb36b363a5af1cfd0141ccc142b96f71
     }
   }
 }
