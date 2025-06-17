8d063b3a19ac38a0913a1fa88a152941d630f316
Jorge Herrera <jorgeluishm88@gmail.com>
2025-06-17 17:27:12 -0300

    Add file upload handling to worker creation and update routes

--- src/routes/worker.routes.js
+++ src/routes/worker.routes.js
@@ -5,16 +5,19 @@ const validate = require("../middleware/validationsYup");
 const workerValidation = require("../validations/workerValidation");
 const auth = require("../middleware/auth");
 const makeAllFieldsOptional = require("../utils/yupUtils");
+const upload = require("../middleware/upload");
 
 workerRoutes.get("/:id", WorkerController.getWorkerById);
 workerRoutes.post(
   "/",
+  upload.single("file"),
   validate(workerValidation),
   WorkerController.createWorker
 );
 workerRoutes.put(
   "/:id",
   auth,
+  upload.single("file"),
   validate(makeAllFieldsOptional(workerValidation)),
   WorkerController.updateWorker
 );
